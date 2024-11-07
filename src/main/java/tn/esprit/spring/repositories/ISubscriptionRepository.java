3885140362e5c1729cae49c7de4000445f4dc1b1
Wwiss <wissem.zarrami@esprit.tn>
2024-11-07 01:57:51 +0100

    test

--- src/main/java/tn/esprit/spring/repositories/ISubscriptionRepository.java
+++ src/main/java/tn/esprit/spring/repositories/ISubscriptionRepository.java
@@ -2,7 +2,6 @@ package tn.esprit.spring.repositories;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-
 import org.springframework.data.repository.query.Param;
 import tn.esprit.spring.entities.Subscription;
 import tn.esprit.spring.entities.TypeSubscription;
@@ -13,17 +12,14 @@ import java.util.Set;
 
 public interface ISubscriptionRepository extends CrudRepository<Subscription, Long> {
 
-    @Query("select s from Subscription s where s.typeSub = :typeS order by s.startDate")
+    @Query("SELECT s FROM Subscription s WHERE s.typeSub = :typeS ORDER BY s.startDate")
     Set<Subscription> findByTypeSubOrderByStartDateAsc(@Param("typeS") TypeSubscription typeSub);
 
     List<Subscription> getSubscriptionsByStartDateBetween(LocalDate date1, LocalDate date2);
 
-    @Query("select distinct s from Subscription s where s.endDate <= CURRENT_TIME order by s.endDate")
+    @Query("SELECT DISTINCT s FROM Subscription s WHERE s.endDate <= CURRENT_DATE ORDER BY s.endDate")
     List<Subscription> findDistinctOrderByEndDateAsc();
 
-
-    @Query("select (sum(s.price))/(count(s)) from Subscription s where s.typeSub = ?1")
-    Float recurringRevenueByTypeSubEquals(TypeSubscription typeSub);
-
-
+    @Query("SELECT (SUM(s.price) / COUNT(s)) FROM Subscription s WHERE s.typeSub = :typeSub")
+    Float recurringRevenueByTypeSubEquals(@Param("typeSub") TypeSubscription typeSub);
 }
--- src/main/java/tn/esprit/spring/services/SubscriptionServicesImpl.java
+++ src/main/java/tn/esprit/spring/services/SubscriptionServicesImpl.java
@@ -1,5 +1,6 @@
 package tn.esprit.spring.services;
 
+
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
